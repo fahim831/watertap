3657b24d4c50ecf4674b1f0edde2438c60260d6e
fahim831 <44279032+fahim831@users.noreply.github.com>
2024-11-12 16:11:20 -0500

    Working pyomo model

---
+++ watertap/fahim/ro_1d_dae_constant_T.py
@@ -0,0 +1,438 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2024
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+# Example 1 from http://www.mathworks.com/help/matlab/ref/pdepe.html
+
+from pyomo.environ import *
+from pyomo.dae import *
+from enum import Enum
+import idaes.core.util.scaling as iscale
+import idaes.logger as idaeslog
+from idaes.core.solvers import petsc
+from idaes.core.util.model_statistics import degrees_of_freedom
+
+class Solver(Enum):
+    ipopt = 1
+    petsc = 2
+
+SolverChoice = Solver.ipopt
+if SolverChoice == Solver.ipopt:
+    scaling = 1
+else:
+    scaling = 0
+
+m = ConcreteModel()
+m.tf = Param(initialize=0.8e-3) # 8 mm
+m.tp = Param(initialize=0.5e-3) # 5 mm
+m.L = Param(initialize=0.934)
+m.W = Param(initialize=8.4)
+m.rhow = Param(initialize=55.56) # kmol/m3
+m.R = Param(initialize=0.082) # atm m3/K kmol
+m.Pp = Param(initialize=1) # atm
+m.b = Param(initialize=8529.45) # atm s/m4
+m.Aw = Param(initialize=9.5188e-7) # m/atm s
+m.Bs = Param(initialize=8.468e-8) # m/s
+
+# Initial and left boundary conditions
+m.Cb0 = Param(initialize=6.226e-3) # kmol/m3
+m.Cp0 = Param(initialize=0.0) # kmol/m3
+m.Fb0 = Param(initialize=2.166e-4) # m3/s
+m.Pb0 = Param(initialize=5.83) # atm
+m.Tb  = Param(initialize=304.15) # K
+m.Tp  = Param(initialize=304.15) # K
+m.Db  = Param(initialize=1.7657e-9) # m2/s
+m.Dp  = Param(initialize=1.7354e-9) # m2/s
+m.Jw0 = Param(initialize=3.14472750e-06)
+m.Js0 = Param(initialize=5.17374784e-09)
+m.Cw0 = Param(initialize=2.10976363e-02)
+m.kx0 = Param(initialize=1.37700821e-06)
+
+m.t = ContinuousSet(bounds=(0, 100))
+m.x = ContinuousSet(bounds=(0, 1))
+m.Cb = Var(m.x, m.t, within=NonNegativeReals, initialize=m.Cb0)
+m.Cp = Var(m.x, m.t, within=NonNegativeReals, initialize=m.Cp0)
+m.Fb = Var(m.x, m.t, within=NonNegativeReals, initialize=m.Fb0)
+m.Pb = Var(m.x, m.t, within=NonNegativeReals, initialize=m.Pb0)
+m.Jw = Var(m.x, m.t, within=NonNegativeReals, initialize=m.Jw0)
+m.Js = Var(m.x, m.t, within=NonNegativeReals, initialize=m.Js0)
+m.Cw = Var(m.x, m.t, within=NonNegativeReals, initialize=m.Cw0)
+m.kx = Var(m.x, m.t, within=NonNegativeReals, initialize=m.kx0)
+# m.Fp = Var(m.x, m.t, within=NonNegativeReals, initialize=0)
+
+m.dCbdt = DerivativeVar(m.Cb, wrt=m.t, initialize=-1.0)
+m.dCpdt = DerivativeVar(m.Cp, wrt=m.t, initialize=1.0)
+m.dFbdt = DerivativeVar(m.Fb, wrt=m.t, initialize=-1.0)
+m.dPbdt = DerivativeVar(m.Pb, wrt=m.t, initialize=-1.0)
+m.dCbdx = DerivativeVar(m.Cb, wrt=m.x, initialize=-1.0)
+m.dCpdx = DerivativeVar(m.Cp, wrt=m.x, initialize=1.0)
+m.dFbdx = DerivativeVar(m.Fb, wrt=m.x, initialize=-1.0)
+m.dPbdx = DerivativeVar(m.Pb, wrt=m.x, initialize=-1.0)
+m.dCbdx2 = DerivativeVar(m.Cb, wrt=(m.x, m.x), initialize=1.0)
+m.dCpdx2 = DerivativeVar(m.Cp, wrt=(m.x, m.x), initialize=1.0)
+# m.dFpdx = DerivativeVar(m.Fp, wrt=m.x, initialize=1.0)
+
+def _diffeq1(m, i, j): # Cb PDE
+    if i == 0 or i == 1 or j == 0:
+        return Constraint.Skip
+    return m.dCbdt[i, j] == -(m.Cb[i, j]*m.dFbdx[i, j] + m.Fb[i, j]*m.dCbdx[i, j])/(m.tf*m.W) + m.Db*m.dCbdx2[i, j] - m.Jw[i, j]*m.Cp[i, j]/m.tf
+
+m.diffeq1 = Constraint(m.x, m.t, rule=_diffeq1)
+
+def _diffeq2(m, i, j): # Cp PDE
+    if i == 0 or i == 1 or j == 0:
+        return Constraint.Skip
+    return m.dCpdt[i, j] == (m.Cp[i, j]*m.dFbdx[i, j] - (m.Fb0 - m.Fb[i, j])*m.dCpdx[i, j])/(m.tp*m.W) + m.Dp*m.dCpdx2[i, j] + m.Jw[i, j]*m.Cp[i, j]/m.tf
+    # return m.dCpdt[i, j] == -(m.Cp[i, j]*m.dFpdx[i, j] + m.Fp[i, j]*m.dCpdx[i, j])/(m.tp*m.W) + m.Dp[i, j]*m.dCpdx2[i, j] + m.Jw[i, j]*m.Cp[i, j]/m.tf
+
+m.diffeq2 = Constraint(m.x, m.t, rule=_diffeq2)
+
+def _diffeq3(m, i, j): # Fb PDE
+    if i == 0 or j == 0:
+        return Constraint.Skip
+    return m.dFbdt[i, j] == (-m.W*m.Jw[i, j] - m.dFbdx[i, j])*m.Fb[i, j]/(m.tf*m.W)
+
+m.diffeq3 = Constraint(m.x, m.t, rule=_diffeq3)
+
+def _diffeq4(m, i, j): # Pb PDE
+    if i == 0 or j == 0:
+        return Constraint.Skip
+    return m.dPbdt[i, j] == (-m.b*m.Fb[i, j] - m.dPbdx[i, j])*m.Fb[i, j]/(m.tf*m.W)
+
+m.diffeq4 = Constraint(m.x, m.t, rule=_diffeq4)
+
+# def _diffeq7(m, i, j): # Fp PDE for dx
+#     if i == 0 or j == 0:
+#         return Constraint.Skip
+#     return m.dFpdx[i, j] == m.W*m.Jw[i, j]
+
+# m.diffeq7 = Constraint(m.x, m.t, rule=_diffeq7)
+
+def _algeq1(m, i, j): # Jw Algebraic Equation
+    return m.Jw[i, j] == m.Aw*(m.Pb[i, j] - m.Pp - m.R*m.Tb*(m.Cw[i, j] - m.Cp[i, j]))
+
+m.algeq1 = Constraint(m.x, m.t, rule=_algeq1)
+
+def _algeq2(m, i, j): # Js Algebraic Equation
+    return m.Js[i, j] == m.Bs*(m.Cw[i, j] - m.Cp[i, j])
+
+m.algeq2 = Constraint(m.x, m.t, rule=_algeq2)
+
+def _algeq3(m, i, j): # Cw Algebraic Equation
+    return m.Cw[i, j] == m.Cp[i, j] + exp(m.Jw[i, j]/m.kx[i, j])*(m.Cb[i, j] - m.Cp[i, j])
+
+m.algeq3 = Constraint(m.x, m.t, rule=_algeq3)
+
+def _algeq4(m, i, j): # kx Algebraic Equation
+    # return m.kx[i, j] == 0.0344832*m.Jw[i, j]**0.739*m.Cb[i, j]**0.135
+    return m.kx[i, j] == 0.095360572*m.Fb[i, j]**0.13*m.Jw[i, j]**0.739*m.Cb[i, j]**0.135
+    # return m.kx[i, j] == 147.4/(2*m.tf)*m.Db[i, j]*2100*(m.Cb[i, j]/m.rhow)**0.135
+
+m.algeq4 = Constraint(m.x, m.t, rule=_algeq4)
+
+# def _algeq5(m, i, j): # Fp Algebraic Equation
+#     if i == 0 and j != 0:
+#         return Constraint.Skip
+#     # return m.Fp[i, j] == m.Fb0 - m.Fb[i, j]
+#     return m.Fp[i, j]*m.Cp[i, j] == m.Fb0*m.Cb0 - m.Fb[i, j]*m.Cb[i, j]
+
+# m.algeq5 = Constraint(m.x, m.t, rule=_algeq5)
+
+def _initcon_1(m, i): # i=x
+    if i == 1:
+        return Constraint.Skip
+    return m.Cb[i, 0] == m.Cb0
+
+m.initcon_1 = Constraint(m.x, rule=_initcon_1)
+
+def _initcon_2(m, i):
+    if i == 1:
+        return Constraint.Skip
+    return m.Cp[i, 0] == m.Cp0
+
+m.initcon_2 = Constraint(m.x, rule=_initcon_2)
+
+def _initcon_3(m, i):
+    return m.Fb[i, 0] == m.Fb0
+
+m.initcon_3 = Constraint(m.x, rule=_initcon_3)
+
+def _initcon_4(m, i):
+    return m.Pb[i, 0] == m.Pb0
+
+m.initcon_4 = Constraint(m.x, rule=_initcon_4)
+
+def _leftbccon_1(m, j): # j=t
+    if j == 0: # To avoid (0,0) as already specified in IC
+        return Constraint.Skip
+    return m.Cb[0, j] == m.Cb0
+
+m.leftbccon_1 = Constraint(m.t, rule=_leftbccon_1)
+
+def _leftbccon_2(m, j):
+    if j == 0: # To avoid (0,0) as already specified in IC
+        return Constraint.Skip
+    return m.Cp[0, j] == m.Cp0
+
+m.leftbccon_2 = Constraint(m.t, rule=_leftbccon_2)
+
+def _leftbccon_3(m, j):
+    if j == 0: # To avoid (0,0) as already specified in IC
+        return Constraint.Skip
+    return m.Fb[0, j] == m.Fb0
+
+m.leftbccon_3 = Constraint(m.t, rule=_leftbccon_3)
+
+def _leftbccon_4(m, j):
+    if j == 0: # To avoid (0,0) as already specified in IC
+        return Constraint.Skip
+    return m.Pb[0, j] == m.Pb0
+
+m.leftbccon_4 = Constraint(m.t, rule=_leftbccon_4)
+
+def _upperbound_1(m, j):
+    return m.dCbdx[1, j] == 0
+
+m.upperbound_1 = Constraint(m.t, rule=_upperbound_1)
+
+def _upperbound_2(m, j):
+    return m.dCpdx[1, j] == 0
+
+m.upperbound_2 = Constraint(m.t, rule=_upperbound_2)
+
+m.obj = Objective(expr=1)
+
+# Discretize using Orthogonal Collocation
+# discretizer = TransformationFactory('dae.collocation')
+# discretizer.apply_to(m,nfe=5,ncp=3,wrt=m.x)
+# discretizer.apply_to(m,nfe=10,ncp=3,wrt=m.t)
+# print("Degrees of Freedom before Discretization = ", degrees_of_freedom(m))
+# Discretize using Finite Difference and Collocation
+discretizer = TransformationFactory('dae.collocation')
+discretizer2 = TransformationFactory('dae.finite_difference')
+discretizer.apply_to(m, nfe=5, ncp=2, wrt=m.x)
+discretizer2.apply_to(m, nfe=30, wrt=m.t, scheme='BACKWARD')
+
+# Discretize using Finite Difference Method
+# discretizer = TransformationFactory('dae.finite_difference')
+# discretizer.apply_to(m,nfe=6,wrt=m.x,scheme='BACKWARD')
+# discretizer.apply_to(m,nfe=120,wrt=m.t,scheme='BACKWARD')
+
+if scaling == 1:
+    # create the scaling factors
+    m.scaling_factor = Suffix(direction=Suffix.EXPORT)
+    m.scaling_factor[m.diffeq1] = 1e4 # scale Cb eq
+    m.scaling_factor[m.diffeq2] = 1e4 # scale Cp eq
+    m.scaling_factor[m.diffeq3] = 1e5 # scale Fb eq
+    # m.scaling_factor[m.diffeq3] = 1e1 # scale Pb eq
+    m.scaling_factor[m.algeq1] = 1e7 # scale Jw eq
+    m.scaling_factor[m.algeq2] = 1e10  # scale Js eq
+    m.scaling_factor[m.algeq4] = 1e7  # scale kx eq
+    m.scaling_factor[m.Cb] = 1e4    # scale the Cb variable
+    m.scaling_factor[m.Cp] = 1e6    # scale the Cp variable
+    m.scaling_factor[m.Fb] = 1e5    # scale the Fb variable
+    m.scaling_factor[m.Jw] = 1e7    # scale the Jw variable
+    m.scaling_factor[m.Js] = 1e10    # scale the Js variable
+    m.scaling_factor[m.kx] = 1e7    # scale the kx variable
+    # m.scaling_factor[m.dCbdt] = 1e3    # scale the Cb variable
+    # m.scaling_factor[m.dCpdt] = 1e3    # scale the Cp variable
+    # m.scaling_factor[m.dFbdt] = 1e3    # scale the Fb variable
+    # m.scaling_factor[m.dPbdt] = 1e3    # scale the Pb variable
+    # m.scaling_factor[m.dCbdx] = 1e3    # scale the Cb variable
+    # m.scaling_factor[m.dCpdx] = 1e3    # scale the Cp variable
+    iscale.calculate_scaling_factors(m)
+    scaled_model = TransformationFactory('core.scale_model').create_using(m)
+    print("Degrees of Freedom after Discretization = ", degrees_of_freedom(m))
+
+if SolverChoice == Solver.ipopt:
+    with SolverFactory("ipopt") as solver:
+        print(solver._version)
+        solver.options.option_file_name = "solveroptions.opt"
+        with open("solveroptions.opt", "w") as f:
+            f.write("linear_solver ma27\n")
+            f.write("resto_penalty_parameter 20000\n")
+            # f.write("delta 10\n")
+            # f.write("s_theta 5\n")
+            # f.write("gamma_theta 1e-3\n")
+            # f.write("start_with_resto yes\n")
+            # f.write("constraint_violation_norm_type max-norm\n")
+            f.write("constr_viol_tol 1e-3\n")
+            f.write("nlp_scaling_method user-scaling\n")
+            # f.write("max_iter 3\n")
+            f.write("tol 1e-8\n")
+            f.write("check_derivatives_for_naninf yes\n")
+            f.write("print_info_string yes\n")
+            # f.write("print_level 6\n")
+            # f.write("halt_on_ampl_error no\n")
+        if scaling == 1:
+            results = solver.solve(scaled_model, tee=True)
+        else:
+            results = solver.solve(m, tee=True)
+    # assert_optimal_termination(results)
+
+else:
+    idaeslog.solver_log.tee = True
+    results = petsc.petsc_dae_by_time_element(
+        m,
+        time=m.t,
+        keepfiles=True,
+        symbolic_solver_labels=True,
+        ts_options={
+            "--ts_type": "beuler",
+            # "-ts_arkimex_type": "1bee",
+            "--ts_dt": 0.01,
+            "--ts_rtol": 1e-10,
+            # "--ts_adapt_clip":"0.001,3600",
+            # "--ksp_monitor":"",
+            "--ts_adapt_dt_min": 1e-3,
+            "--ts_adapt_dt_max": 3600,
+            "--snes_type": "newtontr",
+            # "--ts_max_reject": 200,
+            # "--ts_monitor": "",
+            # "-ts_adapt_monitor": "",
+            # "--snes_monitor":"",
+            # "-snes_converged_reason": "",
+            # "-ksp_monitor_true_residual": "",
+            # "-ksp_converged_reason": "",
+            # "-snes_test_jacobian": "",
+            # "snes_grid_sequence": "",
+            "-pc_type": "lu",
+            # "-mat_view": "",
+            "--ts_save_trajectory": 1,
+            "--ts_trajectory_type": "visualization",
+            "--ts_max_snes_failures": 25,
+            # "--show_cl":"",
+            "-snes_max_it": 50,
+            "-snes_rtol": 0,
+            "-snes_stol": 0,
+            "-snes_atol": 1e-10,
+        },
+        skip_initial=False,
+        initial_solver="ipopt",
+        initial_solver_options={
+            "constr_viol_tol": 1e-8,
+            "nlp_scaling_method": "user-scaling",
+            "linear_solver": "mumps",
+            "OF_ma57_automatic_scaling": "yes",
+            "max_iter": 300,
+            "tol": 1e-8,
+            "halt_on_ampl_error": "no",
+        },
+    )
+    for result in results.results:
+        assert_optimal_termination(result)
+
+if scaling == 1: TransformationFactory('core.scale_model').propagate_solution(scaled_model, m)
+
+x  = []
+t  = []
+Cb = []
+Cp = []
+Fb = []
+Pb = []
+Jw = []
+Js = []
+Cw = []
+kx = []
+# Fp = []
+
+for i in sorted(m.x):
+    tempCb = []
+    tempCp = []
+    tempFb = []
+    tempPb = []
+    tempJw = []
+    tempx  = []
+    tempJs = []
+    tempCw = []
+    tempkx = []
+    # tempFp = []
+    for j in sorted(m.t):
+        tempx.append(i)
+        tempCb.append(value(m.Cb[i, j]))
+        tempCp.append(value(m.Cp[i, j]))
+        tempFb.append(value(m.Fb[i, j]))
+        tempPb.append(value(m.Pb[i, j]))
+        tempJw.append(value(m.Jw[i, j]))
+        tempJs.append(value(m.Js[i, j]))
+        tempCw.append(value(m.Cw[i, j]))
+        tempkx.append(value(m.kx[i, j]))
+        # tempFp.append(value(m.Fp[i, j]))
+    x.append(tempx)
+    t.append(sorted(m.t))
+    Cb.append(tempCb)
+    Cp.append(tempCp)
+    Fb.append(tempFb)
+    Pb.append(tempPb)
+    Jw.append(tempJw)
+    Js.append(tempJs)
+    Cw.append(tempCw)
+    kx.append(tempkx)
+    # Fp.append(tempFp)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+x, t, Cb, Cp, Fb, Pb, Jw, Js, Cw, kx = np.array(x), np.array(t), np.array(Cb), np.array(Cp), np.array(Fb), np.array(Pb), np.array(Jw), np.array(Js), np.array(Cw), np.array(kx)
+# Fp = np.array(Fp)
+Fp = np.array(m.Fb0 - Fb)
+Cp_av = np.sum(Cp, axis=0) # axis=0 sums all x at each t
+Cb_xL_t_end = Cb[-1,-1]
+Fb_t_end = Fb[:,-1]
+Fp_t_end = Fp[:,-1]
+Rej_av = (1-Cp_av[-1]/Cb_xL_t_end)*100
+tot_retentate_flow_rate = np.sum(Fb, axis=1) # axis=1 sums all t at each x
+tot_permeate_flow_rate = np.sum(Fp, axis=1) # axis=1 sums all t at each x
+Rec_x = Fp_t_end/m.Fb0*100
+Rec_tot = tot_permeate_flow_rate/m.Fb0*100
+
+print('Jw0 model vs real: ', Jw[0,0], 3.14472750e-06)
+print('Js0 model vs real: ', Js[0,0], 5.17374784e-09)
+print('Cw0 model vs real: ', Cw[0,0], 6.10976363e-02)
+print('kx0 model vs real: ', kx[0,0], 1.37700821e-06)
+print('Min of params error at 0: ', min([abs(Jw[0,0] - 3.14472750e-06), abs(Js[0,0] - 5.17374784e-09), abs(Cw[0,0] - 6.10976363e-02), abs(kx[0,0] - 1.37700821e-06)]))
+print('Cb[x=0,t=1]: ', Cb[0,1])
+print('Cb[x=L,t=End]: ', Cb[-1,-1], '0.00675')
+print('Fb[x=L,t=End]: ', Fb[-1,-1], '0.0001951')
+print('Cpav[t=End]: ', Cp_av, '0.00182')
+print('Cp_tot: ', np.sum(Cp_av))
+print('Rej_av = ', Rej_av, '73.08')
+# print('Eq 7 residual: ', (Cw-Cp)/(Cb-Cp)-np.exp(Jw/kx))
+# print('x: ', x)
+# print('Fb: ', Fb)
+# print('Fp: ', Fp)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+ax.set_xlabel('Distance x')
+ax.set_ylabel('Time t')
+ax.plot_wireframe(x, t, Cb, rstride=1, cstride=1)
+ax = fig.add_subplot(1, 2, 2, projection='3d')
+ax.set_xlabel('Distance x')
+ax.set_ylabel('Time t')
+ax.plot_wireframe(x, t, Jw, rstride=1, cstride=1)
+plt.show(block=False)
+plt.pause(2)
+plt.close()
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlabel('Time t')
+ax.set_ylabel('Cb(x=L)')
+ax.plot(t[0,:], Cb[-1,:])
+# ax.set_xlim([0, 100])
+plt.show(block=False)
+plt.pause(1)
+plt.close()
+print('Fb_t_end: ', Fb_t_end)
+print('Fp_t_end: ', Fp_t_end)
+print('Tot ret+perm outlet Fb+Fp: ', tot_retentate_flow_rate+tot_permeate_flow_rate)
+print('Rec_x: ', Rec_x)
+print('Rec_tot: ', Rec_tot)
